9da009ee07e83a8bf8b6c66886787f1a49c081f5
Morgan Ogola <morganogola@gmail.com>
2024-10-29 11:07:22 +0300

    first commit

---
+++ src/main/java/com/example/mongoEmployee/MongoEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongoEmployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoEmployeeApplication.class, args);
+	}
+
+}
