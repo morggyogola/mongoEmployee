0759664165e965d00e325b25c5b15573b7806ee0
Morgan Ogola <morganogola@gmail.com>
2024-10-30 09:27:47 +0300

    added the get employee by age, calculate salary methods

--- src/main/java/com/example/mongoEmployee/service/EmployeeService.java
+++ src/main/java/com/example/mongoEmployee/service/EmployeeService.java
@@ -7,7 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class EmployeeService {
@@ -17,19 +20,39 @@ public class EmployeeService {
         this.employeeRepository = employeeRepository;
     }
 
-    public ResponseEntity<Employee> addEmployee(EmployeeModel employeeModel) {
+    public Employee addEmployee(EmployeeModel employeeModel) {
         Employee employee = new Employee();
         employee.setName(employeeModel.name());
         employee.setSalary(employeeModel.salary());
         employee.setAge(employeeModel.age());
         employeeRepository.save(employee);
-        return new ResponseEntity<>(employee, HttpStatus.CREATED);
+        return employee;
 
     }
 
-    public ResponseEntity<List<Employee>> getAllEmployees() {
-        List<Employee> employees = employeeRepository.findAll();
-        return new ResponseEntity<>(employees, HttpStatus.OK);
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+
+    }
+
+    public Optional<List<Employee>> getEmployeesByAge(int age) {
+        return Optional.of(employeeRepository.findAll()
+                .stream()
+                .filter(employee -> employee.getAge() >= age)
+                .collect(Collectors.toList()));
+    }
+
+    public Double calculateSalary() {
+        return employeeRepository.findAll()
+                .stream()
+                .mapToDouble(Employee::getSalary)
+                .sum();
+    }
+
+    public Optional<List<Employee>> getEmployeesByNameStartingWith(String letter) {
+        return Optional.of(employeeRepository.findAll().stream()
+                .filter(employee -> employee.getName().startsWith(letter))
+                .collect(Collectors.toList()));
     }
 
 
